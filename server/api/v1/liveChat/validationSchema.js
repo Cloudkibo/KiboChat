97ea365ee854f9157452d876f5a4fde3e944c13d
sania <sania@cloudkibo.com>
2018-10-11 12:17:04 +0500

    Move model and modify livechat controller to kibochat service #10

---
+++ server/api/v1/liveChat/validationSchema.js
@@ -0,0 +1,97 @@
+/*
+This file will contain the validation schemas.
+By separating it from controller, we are cleaning the code.
+Now the middleware will automatically send error response if the payload fails
+*/
+
+exports.createPayload = {
+  type: 'object',
+  properties: {
+    name: {
+      sender_id: 'string',
+      required: true
+    },
+    recipient_id: {
+      type: 'string',
+      required: true
+    },
+    sender_fb_id: {
+      type: 'string',
+      required: true
+    },
+    recipient_fb_id: {
+      type: 'string',
+      required: true
+    },
+    session_id: {
+      type: 'string',
+      required: true
+    },
+    company_id: {
+      type: 'string',
+      required: true
+    },
+    payload: {
+      type: 'string',
+      required: true
+    },
+    replied_by: {
+      type: 'string',
+      required: true
+    }
+  }
+}
+
+exports.updatePayload = {
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string',
+      required: true
+    },
+    urlmeta: {
+      type: 'string',
+      required: true
+    }
+  }
+}
+
+exports.urlMetaPayload = {
+  type: 'object',
+  properties: {
+    url: {
+      type: 'string',
+      required: false
+    }
+  }
+}
+
+exports.searchPayload = {
+  type: 'object',
+  properties: {
+    session_id: {
+      type: 'string',
+      required: true
+    },
+    text: {
+      type: 'string',
+      required: true
+    }
+  }
+}
+
+exports.indexPayload = {
+  type: 'object',
+  properties: {
+    last_id: {
+      type: 'string'
+    },
+    number: {
+      type: 'string',
+      required: true
+    },
+    page: {
+      type: 'string'
+    }
+  }
+}
