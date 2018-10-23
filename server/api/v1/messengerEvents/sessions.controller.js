131e925e19d2680e44ed7d7ba94be9011a803754
anishachhatwani <i130315@nu.edu.pk>
2018-10-23 10:36:04 +0500

    auth work

--- server/api/v1/messengerEvents/sessions.controller.js
+++ server/api/v1/messengerEvents/sessions.controller.js
@@ -10,6 +10,7 @@ const og = require('open-graph')
 const notificationsUtility = require('../notifications/notifications.utility')
 
 exports.index = function (req, res) {
+  logger.serverLog(TAG, `payload received in sessions ${JSON.stringify(req.body)}`)
   res.status(200).json({
     status: 'success',
     description: `received the payload`
