a98cc2908c78b26d3e3d824fcbefa7af74e17808
arveen kumar <arveenkumar55@gmail.com>
2018-10-29 10:30:23 +0500

    #12

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -66,7 +66,7 @@ exports.setup = function (socketio) {
   })
 }
 
-exports.sendToClient = function (data) {
+exports.sendMessageToClient = function (data) {
   logger.serverLog(TAG, `Sending ${data} payload to client using socket.io`)
   // globalSocket.to(data.room_id).emit('[NAME]', data.payload)
   globalSocket.emit('message', data)
