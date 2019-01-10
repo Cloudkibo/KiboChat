e95174dc09a26934ab789b10cddeb03938939dd4
Baqar <bjafri5@gmail.com>
2019-01-10 11:41:11 +0500

    server side work for click to messenger ads

---
+++ server/api/v1.1/jsonAd/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const validate = require('express-jsonschema').validate
+
+const validationSchema = require('./validationSchema')
+const controller = require('./jsonAd.controller')
+// const auth = require('./../../../auth/auth.service')
+
+router.post('/create',
+  // auth.isAuthenticated(),
+  validate({body: validationSchema.create}),
+  controller.create)
+
+router.put('/edit',
+  // auth.isAuthenticated(),
+  validate({body: validationSchema.edit}),
+  controller.edit)
+
+router.get('/',
+// auth.isAuthenticated(),
+  controller.getAll)
+
+router.get('/:id',
+// auth.isAuthenticated(),
+  controller.getOne)
+
+router.delete('/delete/:id',
+// auth.isAuthenticated(),
+  controller.deleteOne)
+
+module.exports = router
