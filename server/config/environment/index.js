082a35a6ce73eaeee8efafcd2e6b851ad93c451d
Sojharo <sojharo@live.com>
2019-01-18 16:23:00 +0500

    local dev setup CloudKibo/KiboPush#4913

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -11,17 +11,9 @@ const all = {
   // Project root path
   root: path.normalize(`${__dirname}/../../..`),
 
-  // Server port
-  port: process.env.PORT || 3000,
-
-  // Secure Server port
-  secure_port: process.env.SECURE_PORT || 8443,
-
   ip: process.env.IP || undefined,
 
   userRoles: ['buyer', 'admin', 'supervisor', 'agent'],
-
-  domain: `${process.env.DOMAIN || 'http://localhost:3000'}`,
 
   // Mongo Options
   mongo: {
@@ -31,12 +23,7 @@ const all = {
       }
     }
   },
-  allowedIps: ['::ffff:165.227.178.70', '::ffff:167.99.56.161', '::ffff:159.65.47.134', '::ffff:159.203.175.244', '::ffff:159.89.185.221', '::ffff:165.227.66.158', '::ffff:104.131.67.58', '::ffff:165.227.130.222'],
-  webhook_ip: process.env.WEBHOOK_IP_ADDRESS || 'localhost',
-  DBLAYER_URL_KIBOCHAT: `${process.env.DB_LAYER_IP_KIBOCHAT}/api/v1`,
-  DBLAYER_URL_KIBOENGAGE: `${process.env.DB_LAYER_IP_KIBOENGAGE}/api/v1`,
-  ACCOUNTS_URL: process.env.NODE_ENV === 'production' ? 'https://accounts.cloudkibo.com/api/v1' : process.env.NODE_ENV === 'staging' ? 'https://saccounts.cloudkibo.com/api/v1' : 'http://localhost:3001/api/v1',
-  KIBOENGAGE_URL: process.env.NODE_ENV === 'production' ? 'https://kiboengage.cloudkibo.com/api/v1' : process.env.NODE_ENV === 'staging' ? 'https://skiboengage.cloudkibo.com/api/v1' : 'http://localhost:3000/api/v1'
+  allowedIps: ['::ffff:165.227.178.70', '::ffff:167.99.56.161', '::ffff:159.65.47.134', '::ffff:159.203.175.244', '::ffff:159.89.185.221', '::ffff:165.227.66.158', '::ffff:104.131.67.58', '::ffff:165.227.130.222']
 }
 
 module.exports = _.merge(
