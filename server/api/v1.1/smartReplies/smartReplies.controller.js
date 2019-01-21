b4aecdeb77529fc48813cae33a5606b8d70c6077
anishachhatwani <i130315@nu.edu.pk>
2019-01-21 13:52:22 +0500

    waiting subscriber

--- server/api/v1.1/smartReplies/smartReplies.controller.js
+++ server/api/v1.1/smartReplies/smartReplies.controller.js
@@ -319,6 +319,8 @@ function sendMessenger (message, pageId, senderId, postbackPayload, botId) {
                     } else {
                       logger.serverLog(TAG, `Response sent to Messenger: ${JSON.stringify(messageData)}`)
                       let talkToHumanPaylod = logicLayer.talkToHumanPaylod(botId, message, postbackPayload)
+                      console.log('talkToHumanPaylod', talkToHumanPaylod)
+                      console.log('botid in controller', botId)
                       needle.post(
                         `https://graph.facebook.com/v2.6/me/messages?access_token=${page.accessToken}`, talkToHumanPaylod, (err, resp) => {
                           if (err) {
