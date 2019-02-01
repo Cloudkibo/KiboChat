7925bed01051f1d082b937746e5f50fff1704506
anishachhatwani <i130315@nu.edu.pk>
2019-02-01 15:21:21 +0500

    delete tags issue

--- server/api/v1/utility/index.js
+++ server/api/v1/utility/index.js
@@ -39,9 +39,4 @@ exports.callApi = (endpoint, method = 'get', body, token, type = 'accounts') => 
       }
     })
   })
-    .catch(error => {
-      return new Promise((resolve, reject) => {
-        reject(error)
-      })
-    })
 }
