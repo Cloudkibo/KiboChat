62d8b8b8431d062654d2a7ff923e7b4d2303706e
Baqar <bjafri5@gmail.com>
2019-03-11 13:49:33 +0500

    fix kibochat subscribers

--- server/api/v1/subscribers/index.js
+++ server/api/v1/subscribers/index.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const auth = require('../../../auth/auth.service')
 const validate = require('express-jsonschema').validate
 const validationSchema = require('./validationSchema')
-
 const controller = require('./subscribers.controller')
 
 router.get('/',
@@ -26,13 +25,13 @@ router.get('/subscribeBack/:id',
   auth.isAuthenticated(),
   controller.subscribeBack)
 
-router.post('/updatePicture',
-  auth.isAuthenticated(),
-  controller.updatePicture)
-
 router.get('/updateData',
   auth.isAuthenticated(),
   controller.updateData)
+
+router.post('/updatePicture',
+  auth.isAuthenticated(),
+  controller.updatePicture)
 
 router.post('/unSubscribe',
   auth.isAuthenticated(),
--- server/api/v1/subscribers/subscribers.datalayer.js
+++ server/api/v1/subscribers/subscribers.datalayer.js
@@ -1,8 +1,8 @@
 // const TagsSubscribers = require('./../../v1/tags_subscribers/tags_subscribers.model')
 
-/* exports.findTaggedSubscribers = (query) => {
-  return TagsSubscribers.find(query)
-    .populate('tagId')
-    .exec()
-}
-*/
+// /* exports.findTaggedSubscribers = (query) => {
+//   return TagsSubscribers.find(query)
+//     .populate('tagId')
+//     .exec()
+// }
+// */
