5273f7f9a53e2fb9ed1fe1309c4c0d259534fb68
Sojharo <sojharo@live.com>
2019-10-03 17:30:47 +0500

    further work on unit test CloudKibo/KiboPush#6714

--- server/config/environment/testing.js
+++ server/config/environment/testing.js
@@ -1,4 +1,3 @@
-
 'use strict'
 
 // Development specific configuration
@@ -6,27 +5,28 @@
 module.exports = {
 
   // Server port
-  port: 3024,
+  port: 3022,
 
   // Secure Server port
-  secure_port: 8444,
+  secure_port: 8442,
 
-  domain: `${process.env.DOMAIN || 'http://localhost:3024'}`,
+  domain: 'http://localhost:3022',
 
   // MongoDB connection options
   mongo: {
     uri: 'mongodb://localhost/kibochat-test'
   },
   seedDB: false,
-
   facebook: {
     clientID: process.env.FACEBOOK_ID || '159385484629940',
     clientSecret: process.env.FACEBOOK_SECRET || '67527aa04570a034b6ff67335d95e91c',
     callbackURL: `${process.env.DOMAIN || 'https://kibopush-sojharo.ngrok.io'}/auth/facebook/callback`
   },
 
-  api_urls: {
-    kibochat: `http://localhost:3030/api/v1`,
-    kiboengage: `http://localhost:3031/api/v1`
-  }
+  webhook_ip: 'https://kibopush-anisha.ngrok.io',
+  DBLAYER_URL_KIBOCHAT: `http://localhost:3030/api/v1`,
+  DBLAYER_URL_KIBOENGAGE: `http://localhost:3031/api/v1`,
+  ACCOUNTS_URL: 'http://localhost:3024/api/v1',
+  KIBOENGAGE_URL: 'http://localhost:3021/api',
+  kibodash: `http://localhost:5050/api/v1`
 }
