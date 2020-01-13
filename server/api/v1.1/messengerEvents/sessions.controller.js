b94699b2e5a79782c9fb13dc0e7dcf399d30641e
Baqar <bjafri5@gmail.com>
2020-01-13 14:09:28 +0500

    revert changes

--- server/api/v1.1/messengerEvents/sessions.controller.js
+++ server/api/v1.1/messengerEvents/sessions.controller.js
@@ -4,6 +4,7 @@ const TAG = 'api/v1/messengerEvents/sessions.controller'
 const LiveChatDataLayer = require('../liveChat/liveChat.datalayer')
 const botController = require('./bots.controller')
 const needle = require('needle')
+const og = require('open-graph')
 const logicLayer = require('./logiclayer')
 const notificationsUtility = require('../notifications/notifications.utility')
 const { record } = require('../../global/messageStatistics')
@@ -41,14 +42,7 @@ exports.index = function (req, res) {
                 })
             }
             logger.serverLog(TAG, `subscriber updated successfully`, 'debug')
-            utility.callApi('subscribers/query', 'post', {_id: subscriber._id})
-              .then(sub => {
-                console.log('new_chat sub', sub)
-                saveLiveChat(page, sub, event)
-              })
-              .catch(error => {
-                logger.serverLog(TAG, `Failed to fetch subscriber ${JSON.stringify(error)}`, 'error')
-              })
+            saveLiveChat(page, subscriber, event)
           })
           .catch(error => {
             logger.serverLog(TAG, `Failed to update session ${JSON.stringify(error)}`, 'error')
@@ -61,6 +55,7 @@ exports.index = function (req, res) {
 }
 function saveLiveChat (page, subscriber, event) {
   record('messengerChatInComing')
+  let chatPayload = logicLayer.prepareLiveChatPayload(event.message, subscriber, page)
   if (subscriber && !event.message.is_echo) {
     botController.respondUsingBot(page, subscriber, event.message.text)
   }
@@ -100,39 +95,38 @@ function saveLiveChat (page, subscriber, event) {
     .catch(error => {
       logger.serverLog(TAG, `Failed to fetch subscriber ${JSON.stringify(error)}`, 'error')
     })
-  logicLayer.prepareLiveChatPayload(event.message, subscriber, page)
-    .then(chatPayload => {
-      if ((event.message && !event.message.is_echo) || (event.message && event.message.is_echo && event.message.metadata !== 'SENT_FROM_KIBOPUSH')) {
+  if ((event.message && !event.message.is_echo) || (event.message && event.message.is_echo && event.message.metadata !== 'SENT_FROM_KIBOPUSH')) {
+    let urlInText = parseUrl(event.message.text)
+    if (urlInText !== null && urlInText !== '') {
+      og(urlInText, function (err, meta) {
+        if (err) return logger.serverLog(TAG, err, 'error')
+        chatPayload.url_meta = meta
         saveChatInDb(page, chatPayload, subscriber, event)
-      }
-    })
+      })
+    } else {
+      saveChatInDb(page, chatPayload, subscriber, event)
+    }
+  }
 }
 function saveChatInDb (page, chatPayload, subscriber, event) {
-  if (
-    Object.keys(chatPayload.payload).length > 0 &&
-    chatPayload.payload.constructor === Object &&
-    !event.message.delivery &&
-    !event.message.read
-  ) {
+  if (Object.keys(chatPayload.payload).length > 0 && chatPayload.payload.constructor === Object) {
     LiveChatDataLayer.createFbMessageObject(chatPayload)
       .then(chat => {
-        if (!event.message.is_echo) {
-          require('./../../../config/socketio').sendMessageToClient({
-            room_id: page.companyId,
-            body: {
-              action: 'new_chat',
-              payload: {
-                subscriber_id: subscriber._id,
-                chat_id: chat._id,
-                text: chatPayload.payload.text,
-                name: subscriber.firstName + ' ' + subscriber.lastName,
-                subscriber: subscriber,
-                message: chat
-              }
+        require('./../../../config/socketio').sendMessageToClient({
+          room_id: page.companyId,
+          body: {
+            action: 'new_chat',
+            payload: {
+              subscriber_id: subscriber._id,
+              chat_id: chat._id,
+              text: chatPayload.payload.text,
+              name: subscriber.firstName + ' ' + subscriber.lastName,
+              subscriber: subscriber,
+              message: chat
             }
-          })
-          sendautomatedmsg(event, page)
-        }
+          }
+        })
+        sendautomatedmsg(event, page)
       })
       .catch(error => {
         logger.serverLog(TAG, `Failed to create live chate ${JSON.stringify(error)}`, 'error')
@@ -317,4 +311,14 @@ function sendautomatedmsg (req, page) {
         }
       })
   }
+}
+function parseUrl (text) {
+  // eslint-disable-next-line no-useless-escape
+  let urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig
+  let onlyUrl = ''
+  if (text) {
+    let testUrl = text.match(urlRegex)
+    onlyUrl = testUrl && testUrl[0]
+  }
+  return onlyUrl
 }
