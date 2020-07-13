b602d0eb13800a513fab8ef09803e336bbd43aa6
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-07-13 12:46:55 +0500

    notifications issue

--- server/api/v1.1/zoomIntegration/zoomIntegration.controller.js
+++ server/api/v1.1/zoomIntegration/zoomIntegration.controller.js
@@ -102,7 +102,7 @@ const _sendNotification = (data) => {
       if (subscriber.is_assigned && subscriber.assigned_to.type === 'team') {
         callApi(`teams/agents/query`, 'post', {companyId: data.companyId, teamId: subscriber.assigned_to.id}, 'accounts', data.authorization)
           .then(agents => {
-            const userIds = agents.map((a) => data.userId !== a.agentId && a.agentId)
+            const userIds = agents.map((a) => data.userId !== a.agentId._id && a.agentId._id)
             saveNotification(
               userIds,
               data.companyId,
