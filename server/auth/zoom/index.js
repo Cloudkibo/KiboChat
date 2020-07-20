9495fa00723424433d489e24a0acece30fe24e2f
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-07-20 14:20:52 +0500

    app installion

--- server/auth/zoom/index.js
+++ server/auth/zoom/index.js
@@ -7,8 +7,15 @@ const { callApi } = require('../../api/v1.1/utility')
 const logger = require('../../components/logger')
 const TAG = 'auth/zoom/index.js'
 const config = require('../../config/environment')
+const auth = require('../auth.service')
 
-router.get('/', (req, res) => {
+router.get('/', auth.isAuthenticated(), (req, res) => {
+  const userId = req.user._id
+  const companyId = req.user.companyId
+  return res.status(200).json({status: 'success', payload: `https://zoom.us/oauth/authorize?response_type=code&client_id=${config.zoomClientId}&redirect_uri=${config.zoomRedirectUri}&state=${userId}-${companyId}`})
+})
+
+router.get('/callback', (req, res) => {
   if (req.query.code && req.query.state) {
     const userContext = req.query.state.split('-')
     const params = {
@@ -25,7 +32,7 @@ router.get('/', (req, res) => {
           zoomApiCaller('get', 'v2/users/me', {}, {type: 'bearer', token: accessToken}, false)
             .then(zoomUser => {
               const dataToSave = prepareZoomUserPayload(accessToken, refreshToken, zoomUser, userContext)
-              callApi('zoomUsers', 'put', {purpose: 'updateOne', match: {companyId: userContext[1], zoomId: zoomUser.id}, updated: dataToSave, upsert: true})
+              callApi('zoomUsers', 'put', {purpose: 'updateOne', match: {companyId: userContext[1]}, updated: dataToSave, upsert: true})
                 .then(saved => {
                   res.redirect('/successMessage')
                 })
@@ -47,8 +54,6 @@ router.get('/', (req, res) => {
         logger.serverLog(TAG, `Failed to fetch accessToken ${err}`)
         res.redirect('/ErrorMessage')
       })
-  } else if (req.query.companyId && req.query.userId) {
-    res.redirect(`https://zoom.us/oauth/authorize?response_type=code&client_id=${config.zoomClientId}&redirect_uri=${config.zoomRedirectUri}&state=${req.query.userId}-${req.query.companyId}`)
   } else {
     logger.serverLog(TAG, 'Parameters are missing')
     res.redirect('/ErrorMessage')
