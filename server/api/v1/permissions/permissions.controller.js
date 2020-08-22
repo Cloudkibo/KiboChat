c373778ab8136bf080d11e4c55fbd967939e0f99
sania <sania@cloudkibo.com>
2020-08-22 12:27:46 +0500

    Endpoints for fetch/update muteNotifications in permissions

--- server/api/v1/permissions/permissions.controller.js
+++ server/api/v1/permissions/permissions.controller.js
@@ -21,6 +21,16 @@ exports.updatePermissions = function (req, res) {
     })
 }
 
+exports.changePermissions = function (req, res) {
+  utility.callApi(`permissions/genericUpdate`, 'post',{query:{companyId: req.user.companyId, userId: req.user._id}, newPayload:req.body.payload, options: {upsert: true}}, 'accounts',  req.headers.authorization)
+    .then(result => {
+      res.status(200).json({status: 'success', payload: 'Changes updated successfully'})
+    })
+    .catch(error => {
+      res.status(500).json({status: 'failed', description: error})
+    })
+}
+
 exports.fetchPermissions = function (req, res) {
   utility.callApi(`companyUser/query`, 'post', {domain_email: req.user.domain_email})
     .then(companyUser => {
@@ -29,6 +39,34 @@ exports.fetchPermissions = function (req, res) {
           res.status(200).json({
             status: 'success',
             description: permissions
+          })
+        })
+        .catch(error => {
+          res.status(500).json({
+            status: 'failed',
+            description: error
+          })
+        })
+    })
+    .catch(error => {
+      res.status(500).json({
+        status: 'failed',
+        description: `Internal Server Error ${JSON.stringify(error)}`
+      })
+    })
+}
+
+exports.fetchUserPermissions = function (req, res) {
+  utility.callApi(`companyUser/query`, 'post', {domain_email: req.user.domain_email})
+    .then(companyUser => {
+      utility.callApi(`permissions/query`, 'post', {companyId: companyUser.companyId, userId: req.user._id})
+        .then(userPermission => {
+          if (userPermission.length > 0) {
+            userPermission = userPermission[0]
+          }
+          res.status(200).json({
+            status: 'success',
+            payload: userPermission
           })
         })
         .catch(error => {
