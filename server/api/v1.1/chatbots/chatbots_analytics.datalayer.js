35a0237096b4a897f53ed15b91c1c4a6defb1b26
Sojharo <sojharo@live.com>
2020-08-23 14:00:24 +0500

    work on chatbot analytics CloudKibo/KiboPush#9471

--- server/api/v1.1/chatbots/chatbots_analytics.datalayer.js
+++ server/api/v1.1/chatbots/chatbots_analytics.datalayer.js
@@ -23,7 +23,29 @@ exports.createForBotAnalytics = (payload) => {
 }
 
 exports.createForBotSubscribersAnalytics = (payload) => {
-  return callApi(`chatbot_subscribers_analytics`, 'post', payload, kibochat)
+  return callApi(`chatbot_subscribers_journey`, 'post', payload, kibochat)
+}
+
+exports.findOneForBotSubscribersAnalytics = (match) => {
+  let query = {
+    match,
+    purpose: 'findOne'
+  }
+  return callApi(`chatbot_subscribers_journey/query`, 'post', query, kibochat)
+}
+
+exports.aggregateForBotSubscribersAnalytics = (match, group, lookup, limit, sort, skip) => {
+  let query = {
+    purpose: 'aggregate',
+    match: match
+  }
+  if (group) query.group = group
+  if (lookup) query.lookup = lookup
+  if (limit) query.limit = limit
+  if (sort) query.sort = sort
+  if (skip) query.skip = skip
+
+  return callApi(`chatbot_subscribers_journey/query`, 'post', query, kibochat)
 }
 
 exports.genericUpdateBotAnalytics = (queryObject, updated, options) => {
