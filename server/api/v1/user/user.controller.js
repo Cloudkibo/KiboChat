f8e51357276b9e8d1e9c23bdcbd6a12527a143a8
arveen kumar <arveenkumar55@gmail.com>
2020-09-02 15:01:34 +0500

    fix

--- server/api/v1/user/user.controller.js
+++ server/api/v1/user/user.controller.js
@@ -304,3 +304,29 @@ function saveShopifyIntegration (shop, shopToken, userId, companyId) {
       logger.serverLog(TAG, 'shopify store integration creation error' + err, 'error')
     })
 }
+
+exports.logout = function (req, res) {
+  utility.callApi(`users/receivelogout`, 'get', {}, 'kiboengage', req.headers.authorization)
+    .then(response => {
+      return res.status(200).json({
+        status: 'success',
+        payload: 'send response successfully!'
+      })
+    }).catch(err => {
+      console.log('error', err)
+      res.status(500).json({status: 'failed', payload: `failed to sendLogoutEvent ${err}`})
+    })
+}
+
+exports.receivelogout = function (req, res) {
+  require('../../../config/socketio').sendMessageToClient({
+    room_id: req.user.companyId,
+    body: {
+      action: 'logout'
+    }
+  })
+  return res.status(200).json({
+    status: 'success',
+    payload: 'recieved logout event!'
+  })
+}
