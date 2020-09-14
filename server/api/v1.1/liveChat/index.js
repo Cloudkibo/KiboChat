53cbf95a23b336863b6078fea78b555dfbed3f70
sania <sania@cloudkibo.com>
2020-09-14 13:31:22 +0500

    Acting as user server work

--- server/api/v1.1/liveChat/index.js
+++ server/api/v1.1/liveChat/index.js
@@ -10,6 +10,7 @@ const { checkSMPStatus } = require('../../global/middleware')
 
 router.post('/',
   auth.isAuthenticated(),
+  auth.isSuperUserActingAsCustomer('write'),
   auth.doesPlanPermitsThisAction('livechat'),
   auth.doesRolePermitsThisAction('livechatPermission'),
   validate({body: validationSchema.createPayload}),
@@ -17,6 +18,7 @@ router.post('/',
 
 router.post('/getUrlMeta',
   auth.isAuthenticated(),
+  auth.isSuperUserActingAsCustomer(),
   auth.doesPlanPermitsThisAction('livechat'),
   auth.doesRolePermitsThisAction('livechatPermission'),
   validate({body: validationSchema.urlMetaPayload}),
@@ -24,6 +26,7 @@ router.post('/getUrlMeta',
 
 router.post('/search',
   auth.isAuthenticated(),
+  auth.isSuperUserActingAsCustomer(),
   auth.doesPlanPermitsThisAction('livechat'),
   auth.doesRolePermitsThisAction('livechatPermission'),
   validate({body: validationSchema.searchPayload}),
@@ -31,12 +34,14 @@ router.post('/search',
 
 router.post('/:subscriber_id',
   auth.isAuthenticated(),
+  auth.isSuperUserActingAsCustomer(),
   auth.doesPlanPermitsThisAction('livechat'),
   auth.doesRolePermitsThisAction('livechatPermission'),
   controller.index)
 
 router.get('/SMPStatus',
   auth.isAuthenticated(),
+  auth.isSuperUserActingAsCustomer(),
   checkSMPStatus(),
   controller.SMPStatus)
 
