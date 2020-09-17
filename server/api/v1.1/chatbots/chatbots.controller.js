3363991424aa9761d3acd13168622afefd05ca92
arveen kumar <arveenkumar55@gmail.com>
2020-09-17 16:01:47 +0500

    hotfix

--- server/api/v1.1/chatbots/chatbots.controller.js
+++ server/api/v1.1/chatbots/chatbots.controller.js
@@ -315,8 +315,6 @@ exports.redirectToUrl = (req, res) => {
 exports.exportData = (req, res) => {
   analyticsDataLayer.findForBotSubscribersAnalyticsForSQL({companyId: req.user.companyId})
     .then(results => {
-      sendSuccessResponse(res, 200, results)
-      console.log('results.length', results.length)
       var subscriberIds = results.map(value => value.subscriberId)
       var unique = subscriberIds.filter((v, i, a) => a.indexOf(v) === i) 
       let subscribersData = []
