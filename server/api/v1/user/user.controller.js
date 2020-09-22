3816648813d23b5f7f4f515238404faf9483e893
arveen kumar <arveenkumar55@gmail.com>
2020-09-22 14:03:51 +0500

    hot_fix

--- server/api/v1/user/user.controller.js
+++ server/api/v1/user/user.controller.js
@@ -275,15 +275,19 @@ exports.disconnectFacebook = function (req, res) {
       } else {
         updated.platform = ''
       }
-      utility.callApi('user/update', 'post', {query: {_id: req.user._id}, newPayload: updated, options: {}})
-        .then(updated => {
-          return res.status(200).json({
-            status: 'success',
-            payload: 'Updated Successfully!'
-          })
-        })
-        .catch(err => {
-          res.status(500).json({status: 'failed', payload: err})
+      utility.callApi(`companyUser/queryAll`, 'post', {companyId: req.user.companyId}, 'accounts')
+        .then(companyUsers => {
+          let userIds = companyUsers.map(companyUser => companyUser.userId._id)
+          utility.callApi(`user/update`, 'post', {query: {_id: {$in: userIds}}, newPayload: updated, options: {multi: true}})
+            .then(data => {
+              sendSuccessResponse(res, 200, 'Updated Successfully!')
+            })
+            .catch(err => {
+              sendErrorResponse(res, 500, err)
+            })               
+        }).catch(err => {
+          logger.serverLog(TAG, JSON.stringify(err), 'error')
+          sendErrorResponse(res, 500, err)
         })
     })
     .catch(err => {
