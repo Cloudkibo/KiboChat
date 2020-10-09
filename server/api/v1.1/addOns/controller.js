c28ba67c4c83e25a0a002a89e65cc1893878b3f7
ImranBinShoukat <imran.shoukat1995@gmail.com>
2020-10-09 13:25:47 +0500

    purchase add on

--- server/api/v1.1/addOns/controller.js
+++ server/api/v1.1/addOns/controller.js
@@ -40,3 +40,41 @@ exports.companyAddOns = function (req, res) {
       sendErrorResponse(res, 500, 'Error occured while fetching company add ons')
     })
 }
+
+exports.purchaseAddOn = function (req, res) {
+  logger.serverLog(TAG, 'purchaseAddOn endpoint of addOns is hit', 'debug')
+  const addOnId = req.params.id
+  let addOn
+  callApi('addOns/query', 'post', {purpose: 'findOne', match: {_id: addOnId}}, 'accounts', req.headers.authorization)
+    .then(data => {
+      addOn = data
+      return callApi(
+        `companyprofile/charge`,
+        'post',
+        {
+          companyId: req.user.companyId,
+          amount: addOn.price,
+          currency: 'usd',
+          description: `This charge is for purchasing ${addOn.feature} AddOn`
+        },
+        'accounts',
+        req.headers.authorization
+      )
+    })
+    .then(response => {
+      return callApi(
+        'companyAddOns',
+        'post',
+        {companyId: req.user.companyId, addOnId, permissions: addOn.permissions},
+        'accounts',
+        req.headers.authorization
+      )
+    })
+    .then(created => {
+      return sendSuccessResponse(res, 200, null, 'AddOn has been purchased successfully!')
+    })
+    .catch(err => {
+      logger.serverLog(TAG, `Error occured while purchasing addon ${err}`, 'error')
+      return sendErrorResponse(res, 500, null, 'An unexpected error occured')
+    })
+}
