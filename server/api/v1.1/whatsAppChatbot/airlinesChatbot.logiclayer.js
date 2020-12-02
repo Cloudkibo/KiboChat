33f9f71f9f9607468af7922e613cf16c5a08906c
Baqar <bjafri5@gmail.com>
2020-12-02 17:16:45 +0500

    fix

--- server/api/v1.1/whatsAppChatbot/airlinesChatbot.logiclayer.js
+++ server/api/v1.1/whatsAppChatbot/airlinesChatbot.logiclayer.js
@@ -98,7 +98,7 @@ exports.getMessageBlocks = (chatbot) => {
   if (chatbot.botLinks && chatbot.botLinks.faqs) {
     messageBlocks[0].payload[0].text += `\n\n${specialKeyText(FAQS_KEY)}`
     messageBlocks[0].payload[0].specialKeys[FAQS_KEY] = { type: STATIC, blockId: faqsId }
-    getFaqsBlock(chatbot, faqsId, messageBlocks, mainMenuId)
+    getFaqsBlock(chatbot, mainMenuId, faqsId, messageBlocks, mainMenuId)
   }
   return messageBlocks
 }
@@ -248,7 +248,7 @@ const getAirportInfoBlock = (chatbot, backId, AirlineProvider, userInput) => {
   }
 }
 
-const getFaqsBlock = (chatbot, blockId, messageBlocks, backId) => {
+const getFaqsBlock = (chatbot, mainMenuId, blockId, messageBlocks, backId) => {
   messageBlocks.push({
     module: {
       id: chatbot._id,
@@ -265,7 +265,7 @@ const getFaqsBlock = (chatbot, blockId, messageBlocks, backId) => {
         componentType: 'text',
         specialKeys: {
           [BACK_KEY]: { type: STATIC, blockId: backId },
-          [HOME_KEY]: { type: STATIC, blockId: chatbot.startingBlockId }
+          [HOME_KEY]: { type: STATIC, blockId: mainMenuId }
         }
       }
     ],
