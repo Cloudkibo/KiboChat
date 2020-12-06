4ceeb9074cdc47d23a6e8e20664500903f0908ab
Sojharo <sojharo@live.com>
2020-12-06 23:56:12 +0500

    airlines api amadeus

--- server/api/v1.1/airlinesChatbot/airlines.controller.js
+++ server/api/v1.1/airlinesChatbot/airlines.controller.js
@@ -24,10 +24,15 @@ exports.fetchChatbot = (req, res) => {
 
 exports.testRoute = async (req, res) => {
   try {
-    const aviation = new AirlinesProviders(airlinesConstants.aviation, {
-      access_key: config.aviationKey
+    // const aviation = new AirlinesProviders(airlinesConstants.aviation, {
+    //   access_key: config.aviationKey
+    // })
+    // const result = await aviation.fetchFlightByNumber('pa204')
+    const amadeus = new AirlinesProviders(airlinesConstants.amadeus, {
+      clientId: config.amadeus.clientId,
+      clientSecret: config.amadeus.clientSecret
     })
-    const result = await aviation.fetchFlightByNumber('pa204')
+    const result = await amadeus.fetchAirportInfo('John')
     sendSuccessResponse(res, 200, result)
   } catch (err) {
     sendErrorResponse(res, 500, `Failed to test aviation api endpoint ${JSON.stringify(err)}`)
