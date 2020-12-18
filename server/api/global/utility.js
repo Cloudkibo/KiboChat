e285570982440217cda0d786576ec15d17d9a3f4
arveen kumar <arveenkumar55@gmail.com>
2020-12-18 13:59:39 +0500

    add_fix_bug

--- server/api/global/utility.js
+++ server/api/global/utility.js
@@ -28,8 +28,6 @@ const openGraphScrapper = function (url) {
     let options = {url: redirectUrl || url}
     ogs(options, (error, results) => {
       if (error) {
-        const message = error || 'unable fetch url open graph info'
-        logger.serverLog(message, `${TAG}: openGraphScrapper`, {}, {url}, 'error')
         reject(results.error)
       } else {
         resolve(results.data)
