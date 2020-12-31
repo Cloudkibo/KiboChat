49e64497001add4c8323de185de8d42906bf5e1c
arveen kumar <arveenkumar55@gmail.com>
2020-12-31 14:09:37 +0500

    fix_hot_fix

--- server/api/v1.1/smsChat/smsChat.controller.js
+++ server/api/v1.1/smsChat/smsChat.controller.js
@@ -55,7 +55,7 @@ exports.index = function (req, res) {
 }
 
 const isUnverfiedTwilioNumber = function (err) {
-  if (err && err.message && err.message.includes('unverified numbers')) {
+  if (err && err.message && err.message.includes('unverified')) {
     return true
   } else {
     return false
@@ -124,24 +124,22 @@ exports.create = function (req, res) {
             }
           ], 10, function (err, results) {
             if (err) {
-              console.log('err.message', err.message)
-              console.log('err', err)
               const message = err || 'Error in async calls while sending message'
-              if (!isUnverfiedTwilioNumber(err)) {
-                logger.serverLog(message, `${TAG}: exports.create`, req.body, {params: req.params, user: req.user}, 'error')
-                sendErrorResponse(res, 500, `Failed to send message ${JSON.stringify(err)}`)  
-              } else {
-                sendErrorResponse(res, 500, `Please verify your number on Twilio Trail account before sending messages.`)
-              }
+              logger.serverLog(message, `${TAG}: exports.create`, req.body, {params: req.params, user: req.user}, 'error')
+              sendErrorResponse(res, 500, `Failed to send message ${JSON.stringify(err)}`)
             } else {
               sendSuccessResponse(res, 200, results[0])
             }
           })
         })
-        .catch(error => {
-          const message = error || 'Failed to send twilio message'
-          logger.serverLog(message, `${TAG}: exports.create`, req.body, {params: req.params, user: req.user}, 'error')
-          sendErrorResponse(res, 500, `Failed to send message ${JSON.stringify(error)}`)
+        .catch(err => {
+          const message = err || 'Failed to send twilio message'
+          if (!isUnverfiedTwilioNumber(err)) {
+            logger.serverLog(message, `${TAG}: exports.create`, req.body, {params: req.params, user: req.user}, 'error')
+            sendErrorResponse(res, 500, `Failed to send message ${JSON.stringify(err)}`)  
+          } else {
+            sendErrorResponse(res, 500, `Please verify your number on Twilio Trail account before sending messages.`)
+          }
         })
     })
     .catch(error => {
