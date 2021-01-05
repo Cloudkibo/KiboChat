b3f2f9fdff2fafea74bd55e0a889ce67309bfa26
anishachhatwani <i130315@nu.edu.pk>
2021-01-05 16:32:45 +0500

    message alerts script

--- server/scripts/notificationsScript.js
+++ server/scripts/notificationsScript.js
@@ -5,65 +5,50 @@ const async = require('async')
 const moment = require('moment')
 
 exports.runLiveChatNotificationScript = function () {
-  // logger.serverLog(TAG, 'runLiveChatNotificationScript')
-  sendNotification(0, 15)
-}
-
-function sendNotification (skipRecords, LimitRecords) {
-  let findAdminAlerts = {
+  let query = {
     purpose: 'aggregate',
     match: {type: 'adminAlert'},
-    skip: skipRecords,
-    limit: LimitRecords
+    limit: 500
   }
-  utility.callApi(`cronStack/query`, 'post', findAdminAlerts, 'kibochat')
-    .then(alerts => {
-      // logger.serverLog(TAG, `alerts ${JSON.stringify(alerts)} ${JSON.stringify(alerts.length)}`)
-      if (alerts.length > 0) {
-        let deleteAlerts = 0
-        async.each(alerts, function (alert, cb) {
-          // logger.serverLog(TAG, `alert ${JSON.stringify(alert)}`)
-          if (alert.payload && alert.payload.notification_interval) {
-            var now = moment(new Date())
-            var sessionTime = moment(alert.datetime)
-            var duration = moment.duration(now.diff(sessionTime))
-            // logger.serverLog(TAG, `duration ${duration.asMinutes()}`)
-            // logger.serverLog(TAG, `notification_interval ${alert.payload.notification_interval}`)
-            if (duration.asMinutes() > alert.payload.notification_interval) {
-              deleteAlerts = deleteAlerts + 1
-              // logger.serverLog(TAG, `duration passed ${duration}`)
-              utility.callApi(`companyUser/queryAll`, 'post', {companyId: alert.payload.companyId}, 'accounts')
-                .then(companyUsers => {
-                  let assignedMembers
-                  // logger.serverLog(TAG, `companyUsers ${JSON.stringify(companyUsers)}`)
-                  if (alert.payload.assignedMembers === 'buyer') {
-                    assignedMembers = companyUsers.filter((companyUser) => companyUser.userId.role === 'buyer')
-                  } else if (alert.payload.assignedMembers === 'admins') {
-                    assignedMembers = companyUsers.filter((companyUser) => companyUser.userId.role === 'admin')
-                  } else {
-                    assignedMembers = companyUsers.filter((companyUser) => companyUser.userId.role !== 'agent')
-                  }
+  unresolvedSession(query)
+}
 
-                  async.each(assignedMembers, function (assignedMember, callback) {
-                    generateAdminNotification(alert, assignedMember, callback)
-                  }, function (err) {
-                    if (err) {
-                      cb(err)
-                    }
-                    deleteCronStackRecord(alert, cb)
-                  })
-                })
-                .catch(err => {
-                  cb(err)
-                })
-            }
+function unresolvedSession (findAdminAlerts) {
+  findAdminAlerts.match['payload.type'] = 'unresolved_session'
+  utility.callApi(`cronStack/query`, 'post', findAdminAlerts, 'kibochat')
+    .then(cronStacks => {
+      if (cronStacks.length > 0) {
+        let deletedAlerts = 0
+        async.each(cronStacks, function (cronStack, cb) {
+          let query = {
+            purpose: 'findOne',
+            match: {companyId: cronStack.payload.companyId, platform: cronStack.payload.platform, type: 'unresolved_session'}
           }
-        }, function (err) {
+          utility.callApi(`alerts/query`, 'post', query, 'kibochat')
+            .then(messageAlert => {
+              if (messageAlert && messageAlert.enabled) {
+                let currentTime = moment(new Date())
+                let sessionTime = moment(cronStack.datetime)
+                let duration = moment.duration(currentTime.diff(sessionTime))
+                // if (duration.asHours() > messageAlert.interval) {
+                  _sendAlerts(cronStack, messageAlert, deletedAlerts, cb)
+                // } else {
+                //   cb()
+                // }
+              } else {
+                _deleteCronStackRecord(cronStack, deletedAlerts, cb)
+              }
+            })
+            .catch(error => {
+              cb(error)
+            })
+        }, function (err, result) {
           if (err) {
-            const message = err || 'Unable to generate admin alerts'
-            logger.serverLog(message, `${TAG}: exports.runLiveChatNotificationScript`, {}, {alerts}, 'error')
+            const message = err || 'Unable to send alerts'
+            logger.serverLog(message, `${TAG}: unresolvedSession`, {}, {cronStacks}, 'error')
           } else {
-            sendNotification(skipRecords + LimitRecords - deleteAlerts, LimitRecords)
+            findAdminAlerts.match['_id'] = {$gt: cronStacks[cronStacks.length - 1]._id}
+            unresolvedSession(findAdminAlerts)
           }
         })
       }
@@ -74,7 +59,101 @@ function sendNotification (skipRecords, LimitRecords) {
     })
 }
 
-function deleteCronStackRecord (alert, cb) {
+function _sendAlerts (cronStack, messageAlert, deletedAlerts, cb) {
+  let query = {
+    purpose: 'findAll',
+    match: {companyId: cronStack.payload.companyId, platform: cronStack.payload.platform}
+  }
+  utility.callApi(`alerts/subscriptions/query`, 'post', query, 'kibochat')
+    .then(subscriptions => {
+      if (subscriptions.length > 0) {
+        let name = cronStack.payload.subscriber.name
+        let notificationMessage = ''
+        if (cronStack.payload.type === 'unresolved_session') {
+          notificationMessage = `Subscriber ${name} session is unresolved and sitting in an open sessions queue for the last ${messageAlert.interval} hour(s).`
+        } else if (messageAlert.payload.type === 'pending_session') {
+          notificationMessage = `Subscriber ${name} session is in pending state for the last ${messageAlert.interval} minute(s) and they are waiting for an agent to respond to them.`
+        } else if (messageAlert.payload.type === 'talk_to_agent') {
+          notificationMessage = `Subscriber ${name} have selected the "Talk to agent" option from {{chatbot name}} chatbot and they are waiting for an agent to respond to them`
+        }
+        let notificationSubscriptions = subscriptions.filter(s => s.alertChannel === 'notification')
+        let messengerSubscriptions = subscriptions.filter(s => s.alertChannel === 'messenger')
+        let whatsAppSubscriptions = subscriptions.filter(s => s.alertChannel === 'whatsApp')
+        let emailSubscriptions = subscriptions.filter(s => s.alertChannel === 'email')
+        let data = {
+          cronStack,
+          messageAlert,
+          notificationSubscriptions,
+          messengerSubscriptions,
+          whatsAppSubscriptions,
+          emailSubscriptions,
+          notificationMessage
+        }
+        async.parallelLimit([
+          _sendInAppNotification.bind(null, data)
+        ], 10, function (err, results) {
+          if (err) {
+            cb(err)
+          } else {
+            _deleteCronStackRecord(cronStack, deletedAlerts, cb)
+          }
+        })
+      } else {
+        _deleteCronStackRecord(cronStack, deletedAlerts, cb)
+      }
+    })
+    .catch(error => {
+      cb(error)
+    })
+}
+
+function _sendInAppNotification (data, next) {
+  if (data.notificationSubscriptions.length > 0) {
+    async.each(data.notificationSubscriptions, function (subscription, callback) {
+      let notification = {
+        companyId: data.messageAlert.companyId,
+        message: data.notificationMessage,
+        agentId: subscription.channelId,
+        category: {type: 'message_alert', id: data.cronStack.payload.subscriber._id},
+        platform: data.messageAlert.platform
+      }
+      utility.callApi(`notifications`, 'post', notification, 'kibochat')
+        .then(savedNotification => {
+          utility.callApi(`permissions/query`, 'post', {companyId: data.messageAlert.companyId, userId: subscription.channelId})
+            .then(userPermission => {
+              if (userPermission.length > 0) {
+                userPermission = userPermission[0]
+              }
+              if (data.cronStack.payload.pageId) {
+                if (userPermission.muteNotifications && userPermission.muteNotifications.includes(data.cronStack.payload.pageId)) {
+                  notification.muteNotification = true
+                } else {
+                  notification.muteNotification = false
+                }
+              } else {
+                notification.muteNotification = false
+              }
+              require('../config/socketio').sendMessageToClient({
+                room_id: data.messageAlert.companyId,
+                body: {
+                  action: 'new_notification',
+                  payload: notification
+                }
+              })
+              next()
+            })
+            .catch(err => {
+              next(err)
+            })
+        })
+        .catch(err => next(err))
+    })
+  } else {
+    next()
+  }
+}
+
+function _deleteCronStackRecord (alert, deletedAlerts, cb) {
   var deleteData = {
     purpose: 'deleteMany',
     match: {
@@ -85,66 +164,10 @@ function deleteCronStackRecord (alert, cb) {
   }
   utility.callApi(`cronstack`, 'delete', deleteData, 'kibochat')
     .then(updatedRecord => {
+      deletedAlerts = deletedAlerts + 1
       cb()
     })
     .catch(err => {
       cb(err)
     })
-}
-
-function generateAdminNotification (alert, user, cb) {
-  // logger.serverLog(TAG, `inside generate admin notification`)
-  var name = ''
-  var notificationMessage = ''
-  if (alert.payload.subscriber.firstName) {
-    name = alert.payload.subscriber.firstName + ' ' + alert.payload.subscriber.lastName
-  } else if (alert.payload.subscriber.name) {
-    name = alert.payload.subscriber.name
-  }
-  if (alert.payload.type === 'unresolvedSession') {
-    notificationMessage = `${name} session is unresolved for the last ${alert.payload.notification_interval} min(s)`
-  } else if (alert.payload.type === 'pendingSession') {
-    notificationMessage = `${name} has been awaiting reply from agent for last ${alert.payload.notification_interval} min(s)`
-  }
-
-  let notification = {
-    companyId: user.companyId,
-    type: alert.payload.type,
-    message: notificationMessage,
-    agentId: user.userId._id,
-    subscriber: alert.payload.subscriber,
-    category: {type: 'message_alert', id: alert.payload.subscriber._id},
-    platform: alert.payload.platform
-  }
-  utility.callApi(`notifications`, 'post', notification, 'kibochat')
-    .then(savedNotification => {
-      utility.callApi(`permissions/query`, 'post', {companyId: user.companyId, userId: user.userId._id})
-        .then(userPermission => {
-          if (userPermission.length > 0) {
-            userPermission = userPermission[0]
-          }
-          if (alert.payload.subscriber.pageId) {
-            if (userPermission.muteNotifications && userPermission.muteNotifications.includes(alert.payload.subscriber.pageId._id)) {
-              notification.muteNotification = true
-            } else {
-              notification.muteNotification = false
-            }
-          } else {
-            notification.muteNotification = false
-          }
-
-          require('../config/socketio').sendMessageToClient({
-            room_id: user.companyId,
-            body: {
-              action: 'new_notification',
-              payload: notification
-            }
-          })
-          cb()
-        })
-        .catch(err => {
-          cb(err)
-        })
-    })
-    .catch(err => cb(err))
 }
