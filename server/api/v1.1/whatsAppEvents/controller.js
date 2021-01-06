93b9863b3d8ac9eda6838c3a3e47e7f37b8384ff
anishachhatwani <i130315@nu.edu.pk>
2021-01-06 14:18:40 +0500

    script

--- server/api/v1.1/whatsAppEvents/controller.js
+++ server/api/v1.1/whatsAppEvents/controller.js
@@ -38,7 +38,6 @@ exports.messageReceived = function (req, res) {
     .then(data => {
       createContact(data)
         .then((isNewContact) => {
-          console.log('isNewContact', isNewContact)
           let number = `+${data.userData.number}`
           if (data.messageData.constructor === Object && Object.keys(data.messageData).length > 0) {
             let query = [
@@ -57,7 +56,6 @@ exports.messageReceived = function (req, res) {
                         // whatsapp chatbot
                         pushUnresolveAlertInStack(company, contact, 'whatsApp')
                         if (isNewContact) {
-                          console.log('insde')
                           _sendEvent(company._id, contact)
                           pushSessionPendingAlertInStack(company, contact, 'whatsApp')
                         }
