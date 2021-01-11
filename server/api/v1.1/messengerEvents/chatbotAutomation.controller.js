999ba79720003bd1aa5b8e9b7795d7f7026053e3
anishachhatwani <i130315@nu.edu.pk>
2021-01-11 11:49:14 +0500

    messageAlerts

--- server/api/v1.1/messengerEvents/chatbotAutomation.controller.js
+++ server/api/v1.1/messengerEvents/chatbotAutomation.controller.js
@@ -17,6 +17,7 @@ const { callApi } = require('../utility')
 const { record } = require('../../global/messageStatistics')
 const { sendWebhook } = require('../../global/sendWebhook')
 const chatbotTemplates = require('../../../chatbotTemplates')
+const { pushTalkToAgentAlertInStack } = require('../../global/messageAlerts')
 
 exports.handleChatBotWelcomeMessage = (req, page, subscriber) => {
   record('messengerChatInComing')
@@ -323,8 +324,7 @@ exports.handleTriggerMessage = (req, page, subscriber) => {
     })
 }
 
-exports.handleChatBotNextMessage = (req, page, subscriber, uniqueId, parentBlockTitle) => {
-  console.log('in handleChatbotNextMessage')
+exports.handleChatBotNextMessage = (req, page, subscriber, uniqueId, parentBlockTitle, payloadAction) => {
   record('messengerChatInComing')
   shouldAvoidSendingAutomatedMessage(subscriber, req)
     .then(shouldAvoid => {
@@ -348,44 +348,49 @@ exports.handleChatBotNextMessage = (req, page, subscriber, uniqueId, parentBlock
                 shouldSend = true
               }
               if (shouldSend) {
-                sendWebhook('CHATBOT_OPTION_SELECTED', 'facebook', {
-                  psid: subscriber.senderId,
-                  pageId: page.pageId,
-                  blockTitle: parentBlockTitle,
-                  option: req.message ? req.message.text : req.postback.title,
-                  chatbotTitle: page.pageName,
-                  timestamp: Date.now()
-                }, page)
-                messageBlockDataLayer.findOneMessageBlock({ uniqueId: uniqueId.toString() })
-                  .then(messageBlock => {
-                    if (messageBlock) {
-                      let blockInfo = {
-                        chatBotId: chatbot._id,
-                        messageBlockId: messageBlock._id,
-                        messageBlockTitle: messageBlock.title
+                if (payloadAction === 'talk_to_agent') {
+                  handleTalkToAgent(page, subscriber)
+                }
+                if (uniqueId) {
+                  sendWebhook('CHATBOT_OPTION_SELECTED', 'facebook', {
+                    psid: subscriber.senderId,
+                    pageId: page.pageId,
+                    blockTitle: parentBlockTitle,
+                    option: req.message ? req.message.text : req.postback.title,
+                    chatbotTitle: page.pageName,
+                    timestamp: Date.now()
+                  }, page)
+                  messageBlockDataLayer.findOneMessageBlock({ uniqueId: uniqueId.toString() })
+                    .then(messageBlock => {
+                      if (messageBlock) {
+                        let blockInfo = {
+                          chatBotId: chatbot._id,
+                          messageBlockId: messageBlock._id,
+                          messageBlockTitle: messageBlock.title
+                        }
+                        senderAction(req.sender.id, 'typing_on', page.accessToken)
+                        intervalForEach(messageBlock.payload, (item) => {
+                          sendResponse(req.sender.id, item, subscriber, page.accessToken, blockInfo)
+                          saveLiveChatMessage(page, subscriber, item)
+                          senderAction(req.sender.id, 'typing_off', page.accessToken)
+                        }, 1500)
+                        if (!isSendingToTester) {
+                          updateBotLifeStatsForBlock(messageBlock, true)
+                          updateBotPeriodicStatsForBlock(chatbot, true)
+                          updateBotSubscribersAnalyticsForSQL(chatbot._id, chatbot.companyId, subscriber, messageBlock)
+                        }
+                        let subscriberLastMessageAt = moment(subscriber.lastMessagedAt)
+                        let dateNow = moment()
+                        if (dateNow.diff(subscriberLastMessageAt, 'days') >= 1 && !isSendingToTester) {
+                          updateBotPeriodicStatsForReturning(chatbot)
+                        }
                       }
-                      senderAction(req.sender.id, 'typing_on', page.accessToken)
-                      intervalForEach(messageBlock.payload, (item) => {
-                        sendResponse(req.sender.id, item, subscriber, page.accessToken, blockInfo)
-                        saveLiveChatMessage(page, subscriber, item)
-                        senderAction(req.sender.id, 'typing_off', page.accessToken)
-                      }, 1500)
-                      if (!isSendingToTester) {
-                        updateBotLifeStatsForBlock(messageBlock, true)
-                        updateBotPeriodicStatsForBlock(chatbot, true)
-                        updateBotSubscribersAnalyticsForSQL(chatbot._id, chatbot.companyId, subscriber, messageBlock)
-                      }
-                      let subscriberLastMessageAt = moment(subscriber.lastMessagedAt)
-                      let dateNow = moment()
-                      if (dateNow.diff(subscriberLastMessageAt, 'days') >= 1 && !isSendingToTester) {
-                        updateBotPeriodicStatsForReturning(chatbot)
-                      }
-                    }
-                  })
-                  .catch(error => {
-                    const message = error || 'error in fetching message block'
-                    return logger.serverLog(message, `${TAG}: exports.handleChatBotNextMessage`, {}, {req, page, subscriber, uniqueId, parentBlockTitle}, 'error')
-                  })
+                    })
+                    .catch(error => {
+                      const message = error || 'error in fetching message block'
+                      return logger.serverLog(message, `${TAG}: exports.handleChatBotNextMessage`, {}, {req, page, subscriber, uniqueId, parentBlockTitle}, 'error')
+                    })
+                }
               }
             }
           })
@@ -802,7 +807,22 @@ const isTriggerMessage = (event, page) => {
       })
   })
 }
-
+function handleTalkToAgent (page, subscriber) {
+  pushTalkToAgentAlertInStack({_id: subscriber.companyId}, subscriber, 'messenger', page.pageName)
+  facebookApiCaller('v6.0', `me/messages?access_token=${page.accessToken}`, 'post', {
+    messaging_type: 'RESPONSE',
+    recipient: JSON.stringify({ id: subscriber.senderId }),
+    message: {
+      text: 'Our support agents have been notified and someone will get back to you soon.',
+      'metadata': 'This is a meta data'
+    }
+  }).then(result => {
+  })
+    .catch(err => {
+      const message = err || 'error in sending action'
+      return logger.serverLog(message, `${TAG}: sendTalkToAgentResponse`, {}, {page, subscriber}, 'error')
+    })
+}
 exports.updateBotPeriodicStatsForBlock = updateBotPeriodicStatsForBlock
 exports.updateBotLifeStatsForBlock = updateBotLifeStatsForBlock
 exports.sendResponse = sendResponse
