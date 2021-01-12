852be8b57968587d9f88b17657fe8fec76408d5e
anishachhatwani <i130315@nu.edu.pk>
2021-01-12 13:37:48 +0500

    cequens

--- server/whatsAppMapper/cequens/cequens.js
+++ server/whatsAppMapper/cequens/cequens.js
@@ -1,11 +1,18 @@
 const logicLayer = require('./logiclayer')
 const { cequensApiCaller } = require('../../api/global/cequensApiCaller')
+const logger = require('../../components/logger')
+const TAG = 'whatsAppMapper/cequens/cequens.js'
 const async = require('async')
 
 exports.sendChatMessage = (data) => {
   return new Promise((resolve, reject) => {
     let MessageObject = logicLayer.prepareSendMessagePayload(data)
-    cequensApiCaller('messages', data.whatsApp.clientName, data.whatsApp.businessNumber, 'post', data.whatsApp.accessToken, MessageObject)
+    cequensApiCaller('messages',
+      data.whatsApp.clientName,
+      data.whatsApp.businessNumber,
+      'post',
+      data.whatsApp.accessToken,
+      MessageObject)
       .then(response => {
         if (response.body.errors) {
           reject(response.body.errors.title)
@@ -31,6 +38,104 @@ exports.verifyCredentials = (body) => {
 }
 exports.getTemplates = (body) => {
   return new Promise((resolve, reject) => {
-    resolve([])
+    let templates = logicLayer.prepareTemplates()
+    resolve(templates)
+  })
+}
+exports.sendInvitationTemplate = (body) => {
+  return new Promise((resolve, reject) => {
+    let requests = []
+    for (let j = 0; j < body.numbers.length; j++) {
+      requests.push(new Promise((resolve, reject) => {
+        setTimeout(() => {
+          cequensApiCaller('messages',
+            body.whatsApp.clientName,
+            body.whatsApp.businessNumber,
+            'post',
+            body.whatsApp.accessToken,
+            logicLayer.prepareInvitationPayload(body, body.numbers[j]))
+            .then(response => {
+              if (response.body.errors) {
+                const message = response.body.errors.title || 'Error while sending invitation message'
+                logger.serverLog(message, `${TAG}: exports.sendInvitationTemplate`, {}, {body}, 'error')
+                resolve()
+              } else {
+                resolve('success')
+              }
+            })
+            .catch(error => {
+              const message = error || 'Error while sending invitation message'
+              logger.serverLog(message, `${TAG}: exports.sendInvitationTemplate`, {}, {body}, 'error')
+              resolve()
+            })
+        }, 1000)
+      }))
+    }
+    Promise.all(requests)
+      .then((responses) => {
+        resolve()
+      })
+      .catch((err) => reject(err))
+  })
+}
+exports.respondUsingChatbot = ({payload, options, company, subscriber}) => {
+  return new Promise((resolve, reject) => {
+    async.eachSeries(payload, function (item, cb) {
+      logicLayer.prepareChatbotPayload(company, subscriber, item, options)
+        .then(message => {
+          cequensApiCaller('messages',
+            company.whatsApp.clientName,
+            company.whatsApp.businessNumber,
+            'post',
+            company.whatsApp.accessToken,
+            message)
+            .then(response => {
+              if (response.body.errors) {
+                cb(response.body.errors)
+              } else {
+                cb()
+              }
+            })
+            .catch(error => {
+              cb(error)
+            })
+        })
+        .catch(err => { cb(err) })
+    }, function (err) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({status: 'success'})
+      }
+    })
+  })
+}
+
+exports.sendTextMessage = ({text, company, subscriber}) => {
+  return new Promise((resolve, reject) => {
+    let MessageObject = {
+      to: subscriber.number.replace(/\D/g, ''),
+      recipient_type: 'individual',
+      type: 'text',
+      text: {
+        body: text
+      }
+    }
+    cequensApiCaller('messages',
+      company.whatsApp.clientName,
+      company.whatsApp.businessNumber,
+      'post',
+      company.whatsApp.accessToken,
+      MessageObject)
+      .then(response => {
+        if (response.body.errors) {
+          reject(response.body.errors.title)
+        } else {
+          resolve({status: 'success'})
+        }
+      })
+      .catch(error => {
+        reject(error)
+      })
   })
 }
