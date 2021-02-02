3810edd8ac03dbb5d60797991fa13902e70f37a6
Baqar <bjafri5@gmail.com>
2021-02-02 13:38:30 +0500

    return order

--- server/api/v1.1/whatsAppChatbot/commerceChatbot.logiclayer.js
+++ server/api/v1.1/whatsAppChatbot/commerceChatbot.logiclayer.js
@@ -52,7 +52,8 @@ const {
   ASK_ORDER_ID,
   GET_INVOICE,
   GET_CHECKOUT_INFO,
-  VIEW_CATALOG
+  VIEW_CATALOG,
+  RETURN_ORDER
 } = require('./constants')
 const { convertToEmoji, sendTalkToAgentNotification } = require('./whatsAppChatbot.logiclayer')
 const logger = require('../../../components/logger')
@@ -352,63 +353,6 @@ const getDiscoverProductsBlock = async (chatbot, backId, EcommerceProvider, inpu
   }
 }
 
-// const getReturnOrderIdBlock = (chatbot, blockId, messageBlocks) => {
-//   messageBlocks.push({
-//     module: {
-//       id: chatbot._id,
-//       type: 'whatsapp_commerce_chatbot'
-//     },
-//     title: 'Get Return Product ID',
-//     uniqueId: blockId,
-//     payload: [
-//       {
-//         text: `Please enter your order id`,
-//         componentType: 'text',
-//         action: { type: DYNAMIC, action: RETURN_ORDER, input: true }
-//       }
-//     ],
-//     userId: chatbot.userId,
-//     companyId: chatbot.companyId
-//   })
-// }
-
-// const getReturnOrderBlock = async (chatbot, backId, EcommerceProvider, orderId) => {
-//   try {
-//     let messageBlock = {
-//       module: {
-//         id: chatbot._id,
-//         type: 'whatsapp_commerce_chatbot'
-//       },
-//       title: 'Return Request',
-//       uniqueId: '' + new Date().getTime(),
-//       payload: [
-//         {
-//           text: dedent(`Your return request has been made.\n
-//             ${specialKeyText(SHOW_CART_KEY)}
-//             ${specialKeyText(BACK_KEY)}
-//             ${specialKeyText(HOME_KEY)}`),
-//           componentType: 'text',
-//           specialKeys: {
-//             [SHOW_CART_KEY]: { type: DYNAMIC, action: SHOW_MY_CART },
-//             [BACK_KEY]: { type: STATIC, blockId: backId },
-//             [HOME_KEY]: { type: STATIC, blockId: chatbot.startingBlockId }
-//           }
-//         }
-//       ],
-//       userId: chatbot.userId,
-//       companyId: chatbot.companyId
-//     }
-
-//     await EcommerceProvider.returnOrder(orderId)
-
-//     return messageBlock
-//   } catch (err) {
-//     const message = err || 'Unable to return order'
-//     logger.serverLog(message, `${TAG}: exports.getReturnOrderBlock`, {}, {}, 'error')
-//     throw new Error(`${ERROR_INDICATOR}Unable to return order. Please make sure your order ID is valid.`)
-//   }
-// }
-
 const getFaqsBlock = (chatbot, blockId, messageBlocks, backId) => {
   messageBlocks.push({
     module: {
@@ -604,7 +548,8 @@ const getOrderStatusBlock = async (chatbot, backId, EcommerceProvider, orderId) 
             [BACK_KEY]: { type: STATIC, blockId: backId },
             [HOME_KEY]: { type: STATIC, blockId: chatbot.startingBlockId },
             'i': { type: DYNAMIC, action: GET_INVOICE, argument: orderId },
-            'o': { type: DYNAMIC, action: VIEW_RECENT_ORDERS }
+            'o': { type: DYNAMIC, action: VIEW_RECENT_ORDERS },
+            'r': { type: DYNAMIC, action: RETURN_ORDER, argument: orderId }
           }
         }
       ],
@@ -671,6 +616,9 @@ const getOrderStatusBlock = async (chatbot, backId, EcommerceProvider, orderId) 
     messageBlock.payload[0].text += `\n\nThis order was placed on ${new Date(orderStatus.createdAt).toDateString()}`
 
     messageBlock.payload[0].text += `\n\n*I*   Get PDF Invoice`
+    if (orderStatus.displayFulfillmentStatus && orderStatus.displayFulfillmentStatus === 'FULFILLED') {
+      messageBlock.payload[0].text += `\n*R*  Request Return for this order`
+    }
     messageBlock.payload[0].text += `\n*O*  View Recent Orders`
     messageBlock.payload[0].text += `\n${specialKeyText(SHOW_CART_KEY)}`
     messageBlock.payload[0].text += `\n${specialKeyText(BACK_KEY)}`
@@ -695,6 +643,42 @@ const getOrderStatusBlock = async (chatbot, backId, EcommerceProvider, orderId) 
     } else {
       throw new Error(`${ERROR_INDICATOR}Unable to get order status.`)
     }
+  }
+}
+
+const getReturnOrderBlock = async (chatbot, backId, EcommerceProvider, orderId) => {
+  try {
+    let messageBlock = {
+      module: {
+        id: chatbot._id,
+        type: 'whatsapp_commerce_chatbot'
+      },
+      title: 'Return Request',
+      uniqueId: '' + new Date().getTime(),
+      payload: [
+        {
+          text: dedent(`Your return request has been made for order #${orderId}.\n
+            ${specialKeyText(SHOW_CART_KEY)}
+            ${specialKeyText(BACK_KEY)}
+            ${specialKeyText(HOME_KEY)}`),
+          componentType: 'text',
+          specialKeys: {
+            [SHOW_CART_KEY]: { type: DYNAMIC, action: SHOW_MY_CART },
+            [BACK_KEY]: { type: STATIC, blockId: backId },
+            [HOME_KEY]: { type: STATIC, blockId: chatbot.startingBlockId }
+          }
+        }
+      ],
+      userId: chatbot.userId,
+      companyId: chatbot.companyId
+    }
+    await EcommerceProvider.returnOrder(Number(orderId))
+
+    return messageBlock
+  } catch (err) {
+    const message = err || 'Unable to return order'
+    logger.serverLog(message, `${TAG}: exports.getReturnOrderBlock`, {}, {}, 'error')
+    throw new Error(`${ERROR_INDICATOR}Unable to return order. Please make sure your order ID is valid.`)
   }
 }
 
@@ -2823,10 +2807,10 @@ exports.getNextMessageBlock = async (chatbot, EcommerceProvider, contact, input)
             messageBlock = await getCheckoutBlock(chatbot, contact.lastMessageSentByBot.uniqueId, EcommerceProvider, contact, action.argument, action.input ? input : '')
             break
           }
-          // case RETURN_ORDER: {
-          //   messageBlock = await getReturnOrderBlock(chatbot, contact.lastMessageSentByBot.uniqueId, EcommerceProvider, action.input ? input : '')
-          //   break
-          // }
+          case RETURN_ORDER: {
+            messageBlock = await getReturnOrderBlock(chatbot, contact.lastMessageSentByBot.uniqueId, EcommerceProvider, action.argument)
+            break
+          }
           case SHOW_ITEMS_TO_REMOVE: {
             messageBlock = await getShowItemsToRemoveBlock(chatbot, contact.lastMessageSentByBot.uniqueId, contact)
             break
