ce83318707c80a6a62da3343d3140539d9864455
sania <sania@cloudkibo.com>
2021-02-04 12:16:03 +0500

    gupshup message events

--- server/whatsAppMapper/gupshup/gupshup.js
+++ server/whatsAppMapper/gupshup/gupshup.js
@@ -1,8 +1,58 @@
-const { gupshupApiCaller } = require('../../api/global/gupshupApiCaller')
 const logicLayer = require('./logiclayer')
 const logger = require('../../components/logger')
 const TAG = 'whatsAppMapper/gupshup/gupshup.js'
 const async = require('async')
+const { callApi } = require('../../api/v1/utility')
+const { gupshupApiCaller } = require('../../api/global/gupshupApiCaller')
+
+exports.getNormalizedMessageReceivedData = (event) => {
+  return new Promise((resolve, reject) => {
+    try {
+      let sender = event.payload.sender
+      let payload = event.payload
+      let query = {'whatsApp.appName': {$regex: event.app, $options: 'i'}}
+      callApi(`companyprofile/query`, 'post', query)
+        .then(company => {
+          if (company) {
+            logicLayer.prepareReceivedMessageData(payload, company)
+              .then(payload => {
+                resolve({
+                  accessToken: company.whatsApp.accessToken,
+                  userData: {
+                    number: sender.phone,
+                    name: sender.name
+                  },
+                  messageData: payload
+                })
+              })
+              .catch(err => {
+                reject(err)
+              })
+          } else {
+            reject(new Error())
+          }
+        })
+        .catch(err => {
+          reject(err)
+        })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+exports.getNormalizedMessageStatusData = (event) => {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve({
+        messageId: event.payload.gsId,
+        status: event.payload.type === 'read' ? 'seen' : event.payload.type
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
 
 exports.sendChatMessage = (data) => {
   return new Promise((resolve, reject) => {
@@ -27,6 +77,7 @@ exports.setWebhook = (body) => {
     resolve()
   })
 }
+
 exports.verifyCredentials = (body) => {
   return new Promise((resolve, reject) => {
     gupshupApiCaller(`template/list/${body.appName}`, 'get', body.accessToken)
@@ -42,6 +93,7 @@ exports.verifyCredentials = (body) => {
       })
   })
 }
+
 exports.getTemplates = (body) => {
   return new Promise((resolve, reject) => {
     gupshupApiCaller(`template/list/${body.whatsApp.appName}`, 'get', body.whatsApp.accessToken)
