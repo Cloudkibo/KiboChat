470d368f19b092417300873ab00e51fa54399f2a
anishachhatwani <i130315@nu.edu.pk>
2021-02-10 09:58:02 +0500

    commmit

--- server/api/v1.1/ecommerceProvidersApiLayer/shopifyProvider.js
+++ server/api/v1.1/ecommerceProvidersApiLayer/shopifyProvider.js
@@ -20,20 +20,6 @@ exports.fetchStoreInfo = (credentials) => {
 }
 
 exports.fetchAllProductCategories = (paginationParams, credentials) => {
-  console.log('inside')
-  const shopify = initShopify(credentials)
-  const shopify1 = new Shopify({
-    shopName: credentials.shopUrl,
-    accessToken: credentials.shopToken,
-    apiVersion: '2021-01'
-  })
-  shopify1.checkout.get()
-    .then(result => {
-      console.log('result from checkout ', result)
-    })
-    .catch(err => {
-      console.log('in catch', err)
-    })
   return new Promise(function (resolve, reject) {
     paginationParams = paginationParams || { limit: 9 }
     shopify.customCollection.list(paginationParams)
@@ -673,20 +659,18 @@ function initShopify (credentials) {
 }
 
 exports.fetchAbandonedCart = (token, credentials) => {
-  console.log('inside fetchAbandonedCart')
   const shopify = initShopify(credentials)
   return new Promise(function (resolve, reject) {
     if (token) {
-      shopify.checkout.list({ limit: 2 })
+      shopify.checkout.get(token)
         .then(result => {
-          console.log('result', result)
           resolve(result)
         })
         .catch(err => {
           reject(err)
         })
     } else {
-      throw new Error('Cart Token is required to complete the order')
+      throw new Error('token is required to complete to get checkout details')
     }
   })
 }
