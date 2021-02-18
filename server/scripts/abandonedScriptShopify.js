20c9d93aab7ca6dece96d485d0e5ae8693ee7e67
saniasiddiqui <31842853+saniasiddiqui@users.noreply.github.com>
2021-02-18 15:06:58 +0500

    remove async with promise

--- server/scripts/abandonedScriptShopify.js
+++ server/scripts/abandonedScriptShopify.js
@@ -16,9 +16,9 @@ exports.runScript = function () {
   let query = { 'commerceCustomerShopify.abandonedCartInfo': { $exists: true, $ne: null } }
   /* Find all contacts with abandoned carts */
   callApi(`whatsAppContacts/query`, 'post', query)
-    .then(async contacts => {
+    .then(contacts => {
       if (contacts.length === 0) return
-      await Promise.all(contacts.map(async (contact) => {
+      Promise.all(contacts.map(async (contact) => {
         try {
           let company = await callApi(`companyProfile/query`, 'post', { _id: contact.companyId })
           if (company && company.whatsApp) {
@@ -62,8 +62,14 @@ exports.runScript = function () {
           return logger.serverLog(message, `${TAG}: exports.runScript`, {contacts, contact}, {}, 'error')
         }
       }))
-      let message = 'Script Run Successfully'
-      logger.serverLog(message, `${TAG}: exports.runScript`, {}, {}, 'info')
+        .then(result => {
+          let message = 'Script Run Successfully'
+          logger.serverLog(message, `${TAG}: exports.runScript`, {}, {}, 'info')
+        })
+        .catch(err => {
+          let message = err || 'Error while running script'
+          logger.serverLog(message, `${TAG}: exports.runScript`, {}, {}, 'error')
+        })
     })
     .catch(error => {
       const message = error || 'Failed to fetch whatsapp contacts'
