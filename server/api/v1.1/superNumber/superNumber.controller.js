ff7be8bcdaa5c67b83950d17b68b3f9aa0094e90
anishachhatwani <i130315@nu.edu.pk>
2021-03-03 17:29:39 +0500

    super number

---
+++ server/api/v1.1/superNumber/superNumber.controller.js
@@ -0,0 +1,13 @@
+const { sendSuccessResponse } = require('../../global/response')
+const {ActionTypes} = require('../../../whatsAppMapper/constants')
+const { whatsAppMapper } = require('../../../whatsAppMapper/whatsAppMapper')
+const { getSuperWhatsAppAccount } = require('../../global/utility')
+
+exports.fetchTemplates = async (req, res) => {
+  let superWhatsAppAccount = getSuperWhatsAppAccount()
+  let templateMessage = whatsAppMapper(
+    superWhatsAppAccount.provider,
+    ActionTypes.GET_COMMERCE_TEMPLATES,
+    {type: req.body.type, language: req.body.language})
+  sendSuccessResponse(res, 200, templateMessage)
+}
