6460d382aaf8bcd8330399fa602553412890afbc
sania <sania@cloudkibo.com>
2021-03-04 15:53:57 +0500

    endpoints for supernumberPreferences

---
+++ server/api/v1.1/superNumber/validationSchema.js
@@ -0,0 +1,44 @@
+/*
+This file will contain the validation schemas.
+By separating it from controller, we are cleaning the code.
+Now the middleware will automatically send error response if the payload fails
+*/
+// For express json validation
+exports.createPayload = {
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'companyId': {
+      'type': 'string'
+    },
+    'abandonedCart': {
+      'type': 'object'
+    },
+    'orderCRM': {
+      'type': 'object'
+    },
+    'cashOnDelivery': {
+      'type': 'object'
+    }
+  },
+  'required': [
+    'companyId'
+  ]
+}
+
+exports.updatePayload =
+{
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'abandonedCart': {
+      'type': 'object'
+    },
+    'orderCRM': {
+      'type': 'object'
+    },
+    'cashOnDelivery': {
+      'type': 'object'
+    }
+  }
+}
