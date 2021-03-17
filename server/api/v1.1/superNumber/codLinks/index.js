2748d086ec2b83f50d1f17a93397d50b21d5fa53
Sojharo <sojharo@live.com>
2021-03-17 10:36:36 +0500

    cod pages content CloudKibo/KiboPush#11117

--- server/api/v1.1/superNumber/codLinks/index.js
+++ server/api/v1.1/superNumber/codLinks/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const dataLayer = require('./../codpages.datalayer')
+const shopifyDataLayer = require('./../../shopify/shopify.datalayer')
+const superNumberDataLayer = require('./../datalayer')
+const commerceConstants = require('./../../ecommerceProvidersApiLayer/constants')
+const EcommerceProviders = require('./../../ecommerceProvidersApiLayer/EcommerceProvidersApiLayer')
 const mainHtml = require('./confirmation.html')
 const logger = require('../../../../components/logger')
 const TAG = 'api/superNumber/codpages.controller.js'
@@ -12,9 +16,20 @@ router.get('/:contactId/:order', (req, res) => {
     contactId,
     order
   })
-    .then(data => {
+    .then(async data => {
       if (data) {
-        res.send(mainHtml.renderHtml(data.storeName, data.order, data.storeType, data.companyId, contactId))
+        const shopifyIntegrations = await shopifyDataLayer.findShopifyIntegrations({
+          companyId: req.body.companyId
+        })
+
+        const ecommerceProvider = new EcommerceProviders(commerceConstants.shopify, {
+          shopUrl: shopifyIntegrations[0].shopUrl,
+          shopToken: shopifyIntegrations[0].shopToken
+        })
+
+        const foundOrder = await ecommerceProvider.checkOrderStatus(req.body.order)
+
+        res.send(mainHtml.renderHtml(data.storeName, data.order, data.storeType, data.companyId, contactId, foundOrder))
       } else {
         res.sendFile(path.join(__dirname, '/404.html'))
       }
