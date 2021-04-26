b83d38a94a33a13acd4b35103b8a19e210cbb0e2
Imran Shoukat <imran.shoukat1995@gmail.com>
2021-04-26 11:01:51 +0500

    mapper incoming

--- server/api/v1.1/smsEvents/index.js
+++ server/api/v1.1/smsEvents/index.js
@@ -4,7 +4,8 @@ const express = require('express')
 const router = express.Router()
 const controller = require('./controller')
 
-router.post('/handleOrderStatus',
-  controller.handleOrderStatus)
+router.post('/handleOrderStatus', controller.handleOrderStatus)
+router.post('/handleIncomingMessage', controller.handleIncomingMessage)
+router.post('/handleMessageDelivery', controller.handleMessageDelivery)
 
 module.exports = router
